9be92f68399f6fb3ad761ad0a01c5534a30febbe
orangenbleu <orangenbleu@gmail.com>
2021-01-30 17:47:29 +0900

    BI, 약관, 뉴스레터, 활동기록 파일 추가

--- src/main/java/com/gymbuddy/backgymbuddy/admin/history/controller/HistoryController.java
+++ src/main/java/com/gymbuddy/backgymbuddy/admin/history/controller/HistoryController.java
@@ -5,41 +5,72 @@ import com.gymbuddy.backgymbuddy.admin.history.domain.History;
 import com.gymbuddy.backgymbuddy.admin.history.service.HistoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static com.gymbuddy.backgymbuddy.admin.base.Constants.HISTORY_PREFIX;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class HistoryController extends BaseController {
 
+    private final String URI_PREFIX = HISTORY_PREFIX;
+
     private final HistoryService historyService;
 
     /**
      * 전체 활동기록 조회
      */
-    @GetMapping("/allHistory")
-    public List<History> selectHistoryList() {
-        return null;
+    @GetMapping(URI_PREFIX + "/allHistory")
+    public ResponseEntity<List<History>> selectHistoryList() {
+        return createResponseEntity(true, historyService.findALl());
     }
 
     /**
      * 활동기록 등록
      */
-    @PostMapping("/newHistory")
-    public Map<String, Object> insertHistory(@RequestBody Map<String, Object> param) {
-        return null;
+    @PostMapping(URI_PREFIX + "/newHistory")
+    public ResponseEntity<Map<String, Object>> insertHistory(@RequestBody History history) {
+        log.info("활동기록 등록: {}", history);
+        Long id = historyService.save(history);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("historyDate", history.getHistoryDate());
+        result.put("title", history.getTitle());
+        return createResponseEntity(true, result);
+    }
+
+    /**
+     * 활동기록 수정
+     */
+    @PutMapping(URI_PREFIX + "/updateHistory/{id}")
+    public ResponseEntity<Map<String, Object>> updateHistory(
+            @PathVariable("id") Long id, @RequestBody Map<String, Object> param) {
+        log.info("활동기록 등록 id: {}, param: {}", id, param);
+        historyService.update(id);
+
+        History findHistory = historyService.findOne(id);
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("historyDate", findHistory.getHistoryDate());
+        result.put("title", findHistory.getTitle());
+        return createResponseEntity(true, result);
     }
 
     /**
      * 활동기록 삭제
-     * @param no 삭제할 글번호
+     * @param id 삭제할 글번호
      */
-    @GetMapping("/deleteHistory/{no}")
-    public Map<String, Object> deleteHistory(@PathVariable("no") int no) {
-        return null;
+    @DeleteMapping(URI_PREFIX + "/deleteHistory/{id}")
+    public ResponseEntity<History> deleteHistory(@PathVariable("id") Long id) {
+        log.info("활동기록 삭제: {}", id);
+        return createResponseEntity(true, historyService.delete(id));
     }
 
 }
--- src/main/java/com/gymbuddy/backgymbuddy/admin/history/service/HistoryService.java
+++ src/main/java/com/gymbuddy/backgymbuddy/admin/history/service/HistoryService.java
@@ -1,10 +1,14 @@
 package com.gymbuddy.backgymbuddy.admin.history.service;
 
+import com.gymbuddy.backgymbuddy.admin.businessIdentity.domain.BusinessIdentity;
+import com.gymbuddy.backgymbuddy.admin.history.domain.History;
 import com.gymbuddy.backgymbuddy.admin.history.repository.HistoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,4 +17,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class HistoryService {
 
     private final HistoryRepository historyRepository;
+
+    public List<History> findALl() {
+        return historyRepository.findAll();
+    }
+
+    public History findOne(Long id) {
+        return historyRepository.findById(id).get();
+    }
+
+    @Transactional
+    public Long save(History history) {
+        historyRepository.save(history);
+        return history.getId();
+    }
+
+    @Transactional
+    public void update(Long id) {
+        History history = historyRepository.findById(id).get();
+    }
+
+    @Transactional
+    public Long delete(Long id) {
+        historyRepository.deleteById(id);
+        return id;
+    }
 }
